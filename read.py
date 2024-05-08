0c5c1968b0cc13f723b845d5d41bd781e128bcb2
nischay-chauhan <masternischay@outlook.com>
2024-05-08 11:20:05 +0530

    REading videos

--- read.py
+++ read.py
@@ -1,7 +1,17 @@
 import cv2 as cv
 
-img = cv.imread('photos/cat_large.jpg')
+# img = cv.imread('photos/cat_large.jpg')
 
-cv.imshow('Cat' , img)
+# cv.imshow('Cat' , img)
 
-cv.waitKey(3000)
+# cv.waitKey(3000)
+
+capture = cv.VideoCapture('videos/dog.mp4')
+
+while True :
+    isTrue , frame = capture.read()
+    cv.imshow('Video' , frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
