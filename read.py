c03358f63aedc78b5f062f24edaf567abae8f466
nischay-chauhan <masternischay@outlook.com>
2024-05-08 11:43:06 +0530

    Rescaling videos

--- read.py
+++ read.py
@@ -1,17 +1,21 @@
 import cv2 as cv
 
-# img = cv.imread('photos/cat_large.jpg')
 
-# cv.imshow('Cat' , img)
-
-# cv.waitKey(3000)
+def rescaleFrame(frame , scale = 0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width , height)
+    return cv.resize(frame , dimensions , interpolation = cv.INTER_AREA)
 
 capture = cv.VideoCapture('videos/dog.mp4')
 
 while True :
     isTrue , frame = capture.read()
+    frame_resized = rescaleFrame(frame)
     cv.imshow('Video' , frame)
+    cv.imshow('Video Resized' , frame_resized)
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
 capture.release()
 cv.destroyAllWindows()
+
